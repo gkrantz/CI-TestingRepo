02f9083fa1c897a2637eb74d8278d09acd9140dd
Sebastian Stiernborg <sebsti@kth.se>
2018-01-26 10:17:36 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Main {
+
+    private Kattio io;
+
+    /**
+     * Constructor.
+     * DO YOUR STUFF HERE.
+     *
+     * @param in  InputStream to handle program input
+     * @param out OutputStream to handle program output
+     */
+    public Main(InputStream in, OutputStream out) {
+        // Set streams
+        this.io = new Kattio(in, out);
+
+        //---------------------
+        // Do stuff here!
+        // use Kattio for input parsing
+        // ex. int n = io.getInt();
+        //---------------------
+
+        // Make sure to close the streams
+        io.close();
+    }
+
+    /**
+     * Main method. Do not write anything here to make it easy to test.
+     * (We want to be able to set in and out stream outside static main.)
+     * @param args main args
+     */
+    public static void main(String args[]) {
+        new Main(System.in, System.out);
+    }
+
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+
+    /**
+     * A basic test to test the complete program.
+     * Input via test1.in
+     * Expected out via test1.out
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testCaseOne() throws Exception {
+
+        // Setup inputstream from file
+        InputStream inFile = new FileInputStream(new File(getClass().getResource("/test1.in").toURI()));
+        // Setup out stream to catch System out
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+        //Run main
+        long t1 = System.currentTimeMillis();
+        new Main(inFile, outContent);
+        long tottime = (System.currentTimeMillis() - t1);
+
+        //Output to string
+        String actual = outContent.toString();
+        String expected = new String(Files.readAllBytes(Paths.get(getClass().getResource("/test1.out").toURI())));
+
+        assertTrue(tottime < 100, () -> "To slow. Runtime: " + tottime + " ms");
+        assertNotEquals(null, actual);
+        assertEquals(expected, actual);
+
+    }
+
+}
